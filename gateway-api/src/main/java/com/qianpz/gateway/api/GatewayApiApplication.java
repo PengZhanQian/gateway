65c88602f978bbc0aa39337927fde3c0858c9cf9
qianpz <qianpz@jcgroup.com.cn>
2018-06-01 10:09:46 +0800

    新增数据库配置信息

--- gateway-api/src/main/java/com/qianpz/gateway/api/GatewayApiApplication.java
+++ gateway-api/src/main/java/com/qianpz/gateway/api/GatewayApiApplication.java
@@ -1,13 +1,9 @@
 package com.qianpz.gateway.api;
 
-import com.qianpz.gateway.dal.config.DataSourceConfig;
-import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan(basePackages = "com.qianpz.gateway")
